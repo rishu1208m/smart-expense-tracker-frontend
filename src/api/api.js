b87c90f0f2eef18a2661789aad84e6729a4e67ff
Your Name <your@email.com>
2026-02-28 20:21:04 +0530

    Updated backend production URL

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://smart-expense-tracker-production-e939.up.railway.app/api",
+  baseURL: "https://smart-expense-tracker-backend-production-a4a5.up.railway.app",
 });
 
 API.interceptors.request.use((req) => {
